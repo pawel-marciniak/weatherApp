304fd6d5ee88e228d2674d4875fdaa5a4d99c73a
p.marciniak <p.marciniak@toucan-systems.pl>
2021-01-23 12:45:52 +0100

    Change caching strategy - cache external API results for 1 minute

--- app/WeatherApi/OpenWeatherMapDriver.php
+++ app/WeatherApi/OpenWeatherMapDriver.php
@@ -10,45 +10,74 @@ use RuntimeException;
 class OpenWeatherMapDriver implements WeatherDriver
 {
     /**
+     * Get weather forecast data
+     *
      * @param string $cityName
      * @param string $units
      * @return WeatherEntity[]
      */
     public function getWeatherForecast(string $cityName, string $units = 'imperial'): iterable
     {
-        $returnData = [];
+        $returnData = Cache::remember("weather:{$cityName}:{$units}", 60, function () use ($cityName, $units) {
+            $weatherData = [];
 
-        try {
-            $response = Http::get('api.openweathermap.org/data/2.5/forecast', [
-                'q' => $cityName,
-                'units' => $units,
-                'appid' => config('weather-api.connections.openWeatherMap.api_key'),
-            ]);
+            try {
+                $data = $this->fetchData($cityName, $units);
 
-            $data = $response->object();
+                if ($data->cod === '200') {
+                    $weatherData = $this->mapData($data->list);
 
-            if ($data->cod === '200') {
-                $returnData = array_map(function ($weather) {
-                    return new WeatherEntity(
-                        $weather->dt_txt,
-                        $weather->main->temp,
-                        $weather->main->pressure,
-                        $weather->main->humidity,
-                    );
-                }, $data->list);
+                    Cache::put("spareWeather:{$cityName}:{$units}", $weatherData);
+                } else {
+                    throw new RuntimeException("Weather API error: {$data->message}");
+                }
+            } catch (Exception $e) {
+                report($e);
 
-                Cache::put("weather:{$cityName}:{$units}", $returnData);
-            } else {
-                throw new RuntimeException("Weather API error: {$data->message}");
+                if (Cache::has("spareWeather:{$cityName}:{$units}")) {
+                    $weatherData = Cache::get("spareWeather:{$cityName}:{$units}");
+                }
             }
-        } catch (Exception $e) {
-            report($e);
 
-            if (Cache::has("weather:{$cityName}:{$units}")) {
-                $returnData = Cache::get("weather:{$cityName}:{$units}");
-            }
-        }
+            return $weatherData;
+        });
 
         return collect($returnData);
+    }
+
+    /**
+     * Fetch data from external API
+     *
+     * @param string $cityName
+     * @param string $units
+     * @return object
+     */
+    protected function fetchData(string $cityName, string $units = 'imperial'): object
+    {
+        $response = Http::get('api.openweathermap.org/data/2.5/forecast', [
+            'q' => $cityName,
+            'units' => $units,
+            'appid' => config('weather-api.connections.openWeatherMap.api_key'),
+        ]);
+
+        return $response->object();
+    }
+
+    /**
+     * Map data to expected format
+     *
+     * @param array $rawWeatherData
+     * @return array
+     */
+    protected function mapData(array $rawWeatherData): array
+    {
+        return array_map(function ($weather) {
+            return new WeatherEntity(
+                $weather->dt_txt,
+                $weather->main->temp,
+                $weather->main->pressure,
+                $weather->main->humidity,
+            );
+        }, $rawWeatherData);
     }
 }
